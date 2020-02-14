2af8ee65b6622013f375260bfb947c1cf75974b2
Chris Dosé <chris.dose@gmail.com>
2020-02-13 23:41:33 -0800

    Install deps

--- mix.exs
+++ mix.exs
@@ -47,6 +47,8 @@ defmodule Xebow.MixProject do
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.6", targets: @all_targets},
       {:nerves_pack, "~> 0.2", targets: @all_targets},
+      {:circuits_gpio, "~> 0.4", targets: @all_targets},
+      {:circuits_spi, "~> 0.1", targets: @all_targets},
 
       # Dependencies for specific targets
       {:nerves_system_rpi0, "~> 1.10", runtime: false, targets: :rpi0}

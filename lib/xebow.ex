d1c3389f2efe08e7029a5b24becaf67b08004b40
Chris Dosé <chris.dose@gmail.com>
2020-02-16 11:24:02 -0800

    cleanup

--- lib/xebow.ex
+++ lib/xebow.ex
@@ -1,18 +1,2 @@
 defmodule Xebow do
-  @moduledoc """
-  Documentation for Xebow.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Xebow.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end

156a996a61a6ec44656cc8faf3bbb1c906d5c413
Alex McLain <alex@alexmclain.com>
2020-04-18 19:16:40 -0700

    Add dialyzer for type checking.

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: [
+    "{mix,.formatter}.exs",
+    "{config,lib,test}/**/*.{ex,exs}",
+    "dialyzer.ignore.exs"
+  ]
 ]

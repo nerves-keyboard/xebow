7560b6614b7e893633a1a2325e78b3703439c526
Alex McLain <alex@alexmclain.com>
2020-05-09 22:01:48 -0700

    Configure unit tests.

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Xebow.MixProject do
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.11", targets: @all_targets, override: true},
       {:nerves_pack, "~> 0.3", targets: @all_targets},
-      {:vintage_net_wizard, "~> 0.2", target: @all_targets},
+      {:vintage_net_wizard, "~> 0.2", target: @all_targets, only: [:dev, :prod]},
       {:circuits_gpio, "~> 0.4", targets: @all_targets},
       {:circuits_spi, "~> 0.1", targets: @all_targets},
       {:usb_gadget, github: "nerves-project/usb_gadget", ref: "master", targets: @all_targets},

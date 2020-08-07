8cb99c9fef39e04f8fd01e4157348e2d9e6d18fa
Alex McLain <alex@alexmclain.com>
2020-08-06 23:36:16 -0700

    Add Animation.type_name.

--- lib/xebow_web/live/animations_live.ex
+++ lib/xebow_web/live/animations_live.ex
@@ -7,42 +7,31 @@ defmodule XebowWeb.AnimationsLive do
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    animations =
-      Animation.types()
-      |> Enum.map(fn animation_module ->
-        module_name =
-          animation_module
-          |> to_string()
-          |> String.split(".")
-          |> List.last()
-
-        id =
-          module_name
-          |> String.replace(~r/([A-Z])/, "_\\1")
-          |> String.downcase()
-          |> String.trim_leading("_")
-
-        name =
-          module_name
-          |> String.replace(~r/([A-Z])/, " \\1")
-          |> String.trim_leading()
-
-        %{
-          module: animation_module,
-          id: id,
-          name: name,
-          active: false
-        }
-      end)
-      |> Enum.sort(&(&1.name < &2.name))
-
-    socket = assign(socket, animations: animations)
-
-    {:ok, socket}
+    {:ok, assign(socket, animations: animations())}
   end
 
   @impl Phoenix.LiveView
   def handle_event("toggle_animation", _params, socket) do
     {:noreply, socket}
+  end
+
+  defp animations do
+    Animation.types()
+    |> Enum.map(fn animation_module ->
+      name = Animation.type_name(animation_module)
+
+      id =
+        name
+        |> String.replace(" ", "_")
+        |> String.downcase()
+
+      %{
+        module: animation_module,
+        id: id,
+        name: name,
+        active: false
+      }
+    end)
+    |> Enum.sort(&(&1.name < &2.name))
   end
 end

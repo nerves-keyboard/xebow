941d81ec167314a0f6a7360a5a88fc4150007098
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-08-12 01:27:17 -0700

    improve error handling and add example field

--- lib/rgb_matrix/animation/config/field_type/integer.ex
+++ lib/rgb_matrix/animation/config/field_type/integer.ex
@@ -3,6 +3,21 @@ defmodule RGBMatrix.Animation.Config.FieldType.Integer do
   An integer field type for use in animation configuration.
 
   Supports defining a minimum and a maximum, as well as a step value.
+
+  To define an integer field in an animation, specify `:integer` as the field
+  type.
+
+  Example:
+      field :speed, :integer,
+        default: 4,
+        min: 0,
+        max: 32,
+        doc: [
+          name: "Speed",
+          description: \"""
+          Controls the speed at which the wave moves across the matrix.
+          \"""
+        ]
   """
 
   @behaviour RGBMatrix.Animation.Config.FieldType
@@ -19,27 +34,30 @@ defmodule RGBMatrix.Animation.Config.FieldType.Integer do
           max: integer,
           doc: keyword(String.t()) | []
         }
+  @type value :: integer
 
   @impl true
-  @spec validate(field_type :: t, value :: integer) :: :ok | :error
+  @spec validate(field_type :: t, value) :: :ok | :validation_error
   def validate(field_type, value) do
     if value >= field_type.min &&
          value <= field_type.max &&
-         mod(value, field_type.step) == 0 do
+         mod(value - field_type.min, field_type.step) == 0 do
       :ok
     else
-      :error
+      :validation_error
     end
   end
 
   @impl true
-  @spec cast(field_type :: t, value :: any) :: {:ok, integer} | :error
+  @spec cast(field_type :: t, any) ::
+          {:ok, value} | :cast_error | :validation_error
   def cast(field_type, value) do
     with {:ok, casted_value} <- do_cast(value),
          :ok <- validate(field_type, casted_value) do
       {:ok, casted_value}
     else
-      :error -> :error
+      :validation_error = e -> e
+      :error -> :cast_error
     end
   end
 
--- lib/rgb_matrix/animation/config/field_type/option.ex
+++ lib/rgb_matrix/animation/config/field_type/option.ex
@@ -3,6 +3,28 @@ defmodule RGBMatrix.Animation.Config.FieldType.Option do
   An option field type for use in animation configuration.
 
   Supports defining a list of pre-defined options as atoms.
+
+  To define an integer field in an animation, specify `:integer` as the field
+  type.
+
+  Example:
+      field :direction, :option,
+        default: :right,
+        options: ~w(right left up down)a, # This must be a list of atoms
+        doc: [
+          name: "Direction",
+          description: \"""
+          Controls the direction of the wave
+          \"""
+        ]
+
+        # Also valid:
+        options: [
+          :right,
+          :left,
+          :up,
+          :down
+        ]
   """
 
   @behaviour RGBMatrix.Animation.Config.FieldType
@@ -16,34 +38,35 @@ defmodule RGBMatrix.Animation.Config.FieldType.Option do
           options: [atom],
           doc: keyword(String.t()) | []
         }
+  @type value :: atom
 
   @impl true
-  @spec validate(field_type :: t, value :: atom) :: :ok | :error
-  def validate(%__MODULE__{options: options}, value) do
+  @spec validate(field_type :: t, value) :: :ok | :validation_error
+  def validate(%__MODULE__{options: options} = _field_type, value) do
     if value in options do
       :ok
     else
-      :error
+      :validation_error
     end
   end
 
   @impl true
-  @spec cast(field_type :: t, value :: any) :: {:ok, atom} | :error
+  @spec cast(field_type :: t, any) ::
+          {:ok, value} | :cast_error | :validation_error
   def cast(field_type, value) do
     with {:ok, casted_value} <- do_cast(value),
          :ok <- validate(field_type, casted_value) do
       {:ok, casted_value}
     else
-      :error -> :error
+      :validation_error = e -> e
+      :error -> :cast_error
     end
   end
 
   defp do_cast(binary_value) when is_binary(binary_value) do
-    try do
-      {:ok, String.to_existing_atom(binary_value)}
-    rescue
-      ArgumentError -> :error
-    end
+    {:ok, String.to_existing_atom(binary_value)}
+  rescue
+    ArgumentError -> :error
   end
 
   defp do_cast(value) when is_atom(value), do: {:ok, value}

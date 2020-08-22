ccd73e5975db95decd9260dbedfd7b81d5825cc9
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-08-22 16:24:49 -0700

    use new errors in Config, minor doc updates

--- lib/rgb_matrix/animation/config.ex
+++ lib/rgb_matrix/animation/config.ex
@@ -34,8 +34,8 @@ defmodule RGBMatrix.Animation.Config do
   `:description`.
 
   The keys are defined by the first atom, the name, provided to an Animation's
-  `field` definition(s). The values are `t:RGBMatrix.Animation.Config.FieldType.t/0`
-  types.
+  `field` definition(s). The values are
+  `t:RGBMatrix.Animation.Config.FieldType.t/0` types.
 
   The documentation is optional and will be initialized to an empty list if
   omitted.
@@ -48,22 +48,14 @@ defmodule RGBMatrix.Animation.Config do
   The keys are defined by the first atom, the name, provided to an Animation's
   `field` definition(s) and must match the field being defined.
 
-  The value should be appropriate for the field.
+  The value should be appropriate for the specified field.
   """
   @type creation_params :: %{optional(atom) => FieldType.value()}
 
   @typedoc """
-  The possible error atoms during creation and update of configs
-  """
-  @type field_error ::
-          :cast_error
-          | :error
-          | :invalid_field
-          | :validation_error
-
-  @typedoc """
-  A tuple of the form `{name, FieldType.t}`, where `name` is one of the
-  valid FieldType names.
+  A tuple of the form, `{name, field}`. `name` is one of the valid
+  `FieldType` names and `field` is a
+  `t:RGBMatrix.Animation.Config.FieldType.t/0` struct.
   """
   @type schema_field :: {name :: atom, FieldType.t()}
 
@@ -83,9 +75,9 @@ defmodule RGBMatrix.Animation.Config do
   @callback update(t, %{optional(atom | String.t()) => any}) :: t
 
   @doc """
-  Returns the map of field types provided by the Config module
+  Returns a map of field types provided by the Config module
   """
-  @spec field_types :: %{atom => FieldType.field()}
+  @spec field_types :: %{atom => FieldType.submodule()}
   def field_types, do: @field_types
 
   defmacro __before_compile__(env) do
@@ -124,15 +116,15 @@ defmodule RGBMatrix.Animation.Config do
   end
 
   @doc """
-  Creates a new `%Config{}` struct belonging to the provided
+  Creates a new `t:t/0` struct belonging to the provided
   `Animation.<type>.Config` module.
 
   The provided Config must be defined through the `use Animation` and `field`
   macros in an `Animation.<type>` module.
 
-  The params provided are a map of type `t:creation_params/0`.
+  The params provided are a map of `t:creation_params/0`.
 
-  Returns a `%Config{}` struct.
+  Returns a `t:t/0` struct.
 
   Example:
       iex> module = RGBMatrix.Animation.HueWave.Config
@@ -142,6 +134,12 @@ defmodule RGBMatrix.Animation.Config do
       %RGBMatrix.Animation.HueWave.Config{direction: :up, speed: 4, width: 30}
 
   The above example shows setting the direction and width to non-default values.
+
+  Any invalid keys in the `t:creation_params/0` map will cause that param to be
+  ignored. Invalid values for fields will be ignored. In both cases, the default
+  provided to the type will be used as the initial value for that field.
+
+  All errors will be logged.
   """
   @spec new_config(
           module :: module,
@@ -149,21 +147,23 @@ defmodule RGBMatrix.Animation.Config do
           params :: creation_params
         ) :: t
   def new_config(module, schema, params) do
-    schema
-    |> Enum.map(fn schema_field -> validate_field(schema_field, params) end)
-    |> Map.new()
-    |> create_struct!(module)
+    schema =
+      schema
+      |> Enum.map(fn schema_field -> validate_field(schema_field, params) end)
+      |> Map.new()
+
+    struct!(module, schema)
   end
 
   @doc """
-  Updates the provided `%Config{}` struct using the provided schema and params.
+  Updates the provided `t:t/0` struct using the provided schema and params.
 
-  The params are a map of type `t:update_params/0`.
+  The params are a map of `t:update_params/0`.
 
   Configs must be retrieved through the use of `Xebow.get_animation_config/0`,
   which will return both the config and the schema.
 
-  Returns the updated `%Config{}` struct.
+  Returns the updated `t:t/0` struct.
 
   Example:
       iex> {config, schema} = Xebow.get_animation_config()
@@ -172,6 +172,9 @@ defmodule RGBMatrix.Animation.Config do
       %RGBMatrix.Animation.HueWave.Config{direction: :left, speed: 8, width: 20}
 
   The above example shows updating the direction and speed.
+
+  Any errors encountered during update are logged, and the struct is returned
+  unchanged.
   """
   @spec update_config(
           config :: t,
@@ -191,64 +194,67 @@ defmodule RGBMatrix.Animation.Config do
           schema :: config_schema
         ) :: t
   defp cast_and_update_field({key, value} = _param, config, schema) do
-    with key <- create_atom_key(key),
-         {:ok, %type_module{} = type} <- Keyword.fetch(schema, key),
+    with {:ok, key} <- create_atom_key(key),
+         {:ok, %type_module{} = type} <- fetch_type_from_schema(schema, key),
          {:ok, value} <- type_module.cast(type, value) do
       Map.put(config, key, value)
     else
-      error ->
-        field_warn(error, key, value)
+      {:error, reason} ->
+        field_warn(reason, key, value)
         config
     end
   end
 
-  @spec validate_field(schema_field, creation_params) ::
-          {atom, FieldType.field()} | nil
-  defp validate_field({key, %type_module{} = type} = _field, params) do
+  @spec validate_field(schema_field, creation_params) :: {atom, FieldType.value()}
+  defp validate_field({key, %type_module{} = type} = _schema_field, params) do
     value = Map.get(params, key, type.default)
 
     case type_module.validate(type, value) do
       :ok ->
         {key, value}
 
-      error ->
-        field_warn(error, key, value)
+      {:error, reason} ->
+        field_warn(reason, key, value)
         {key, type.default}
     end
   end
 
-  @spec create_atom_key(key :: atom | String.t()) ::
-          (key :: atom) | :invalid_field
-  defp create_atom_key(key) when is_atom(key), do: key
+  @spec create_atom_key(key :: atom) ::
+          {:ok, key :: atom} | {:error, :undefined_field}
+  defp create_atom_key(key) when is_atom(key), do: {:ok, key}
 
+  @spec create_atom_key(key :: String.t()) ::
+          {:ok, key :: atom} | {:error, :undefined_field}
   defp create_atom_key(key) when is_binary(key) do
-    String.to_existing_atom(key)
+    {:ok, String.to_existing_atom(key)}
   rescue
     ArgumentError ->
-      :invalid_field
+      {:error, :undefined_field}
   end
 
-  @spec create_struct!(map, module) :: struct
-  defp create_struct!(map, module), do: struct!(module, map)
+  @spec fetch_type_from_schema(config_schema, atom) ::
+          {:ok, FieldType.t()} | {:error, :undefined_field}
+  defp fetch_type_from_schema(schema, key) do
+    case Keyword.fetch(schema, key) do
+      {:ok, type} -> {:ok, type}
+      :error -> {:error, :undefined_field}
+    end
+  end
 
   @spec field_warn(
-          error :: field_error,
+          reason :: FieldType.error(),
           key :: atom | String.t(),
           value :: any
         ) :: :ok
-  defp field_warn(:cast_error, key, value) do
+  defp field_warn(:wrong_type, key, value) do
     Logger.warn("#{inspect(value)} is not the correct type for #{key}")
   end
 
-  defp field_warn(:error, key, _value) do
-    Logger.warn("#{inspect(key)} is an invalid field identifier")
+  defp field_warn(:undefined_field, key, _value) do
+    Logger.warn("#{inspect(key)} is an undefined field identifier")
   end
 
-  defp field_warn(:invalid_field, key, _value) do
-    Logger.warn("#{inspect(key)} is an invalid field identifier")
-  end
-
-  defp field_warn(:validation_error, key, value) do
+  defp field_warn(:invalid_value, key, value) do
     Logger.warn("#{inspect(value)} is an invalid value for #{key}")
   end
 end

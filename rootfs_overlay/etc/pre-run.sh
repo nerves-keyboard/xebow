9bd2cd61488cf3de6d45b8ae2bb51e16554529d7
Alex McLain <alex@alexmclain.com>
2020-08-24 22:55:03 -0700

    Configure USB gadget device before the BEAM boots.

---
+++ rootfs_overlay/etc/pre-run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# ------------------------------------------------------------------------------
+# This pre-run script executes before the BEAM starts.
+# ------------------------------------------------------------------------------
+
+set -e
+
+# Enable the RPi0 USB gadget module.
+modprobe dwc2
+
+# Load USB gadget description.
+gadget-import hidg /etc/usb_gadget/hidg.schema
+echo `ls /sys/class/udc` > /sys/kernel/config/usb_gadget/hidg/UDC
+
+# Bond USB gadget network adapters.
+BOND_DIR="/sys/class/net/bond0/bonding"
+
+ip link set bond0 down
+
+echo "active-backup" > "${BOND_DIR}/mode"
+echo "100" > "${BOND_DIR}/miimon"
+echo "+usb0" > "${BOND_DIR}/slaves"
+echo "+usb1" > "${BOND_DIR}/slaves"
+echo "usb1" > "${BOND_DIR}/primary"
+
+ip link set bond0 up
